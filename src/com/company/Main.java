b8ec0ec5e38dced6c5b28cd421cdb123974cd3f0
Letynochek <yaroslavchek13@mail.ru>
2021-12-08 14:48:55 +0300

    Add Main and Main_Frame

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[]args) {
+        Main_Frame frame = new Main_Frame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
